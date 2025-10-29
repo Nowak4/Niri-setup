66ad2f2f3c9ad230bba171d2721ccaec1ba86e19
Nowak4 <rochamobile1@gmail.com>
2025-10-29 18:22:38 +0100

    niri update

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 sudo pacman -S thunar xarchiver kitty cliphist rofi waybar swaync swww polkit-gnome xwayland-satellite hyprpicker wlogout brightnessctl neovim &&
-yay -S niriswitcher sunsetr-bin swayosd
+paru -S niriswitcher sunsetr-bin swayosd
