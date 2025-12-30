93ced034458c24ce8eb2461dbb8c31e9a5419177
Nowak4 <110620174+Nowak4@users.noreply.github.com>
2025-12-30 16:59:12 +0100

    new configs

---
+++ .config/nvim_1/after/ftplugin/help.lua
@@ -0,0 +1,8 @@
+vim.bo.buflisted = true
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+	pattern = '*',
+	callback = function(event)
+		if vim.bo[event.buf].filetype == 'help' then vim.cmd.only() end
+	end,
+})
